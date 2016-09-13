5ac29f1455e57216f8938fa1156c7e274a9b68e9
Joel Dubowy <jdubowy@gmail.com>
2016-09-13 15:42:12 -0700

    Don't ignore failed imports of args, options, and utils modules when installing

--- afscripting/__init__.py
+++ afscripting/__init__.py
@@ -6,4 +6,9 @@ __author__      = "Joel Dubowy"
 __version_info__ = (1,0,1)
 __version__ = '.'.join([str(n) for n in __version_info__])
 
-from . import args, options, utils
+try:
+    from . import args, options, utils
+except:
+    # This should only happen when __version__ is being
+    # imported in setup.py, so just ignore
+    pass
